0deb756c54ecdf86f9af61c9334268effb2c4c19
Bruno Henrique Crema <brunohcrema@gmail.com>
2022-11-24 06:58:56 -0300

    commit para a entrega de WEB

--- src/edit.php
+++ src/edit.php
@@ -38,29 +38,41 @@
 
     }
 ?>
-<form class="row g-3 needs-validation" method="POST">
-    <div class="row">
-        <div class="col-md-6">
-            <label for="validationCustom01" class="form-label text-secondary">Nome Completo</label>
-            <input type="text" name="input_nome" class="form-control"  value="<?= $user['nome'] ?? '' ?>" required>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Editar Perfil</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
+  </head>
+<body>
+    <form class="row g-3 needs-validation mt-4" method="POST">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="validationCustom01" class="form-label text-secondary">Nome Completo</label>
+                <input type="text" name="input_nome" class="form-control"  value="<?= $user['nome'] ?? '' ?>" required>
+            </div>
+            <div class="col-md-6">
+            <label for="email" class="form-label text-secondary">Email</label>
+            <input type="email" name="input_email" class="form-control" value="<?= $user['email'] ?? '' ?>" required>
+        </div>
+        <div class="col-md-4">
+        <label for="validationCustom03" class="form-label text-secondary">CEP</label>
+            <input type="text" name="input_cep" class="form-control"  value="<?= $user['cep'] ?? '' ?>" required>
+        </div>
+        <div class="col-md-2">
+        <label for="validationCustom04" class="form-label text-secondary">Número</label>
+            <input type="text" name="input_numero" class="form-control"  value="<?= $user['numero'] ?? '' ?>" required>
+
         </div>
         <div class="col-md-6">
-        <label for="email" class="form-label text-secondary">Email</label>
-        <input type="email" name="input_email" class="form-control" value="<?= $user['email'] ?? '' ?>" required>
-    </div>
-    <div class="col-md-4">
-    <label for="validationCustom03" class="form-label text-secondary">CEP</label>
-        <input type="text" name="input_cep" class="form-control"  value="<?= $user['cep'] ?? '' ?>" required>
-    </div>
-    <div class="col-md-2">
-    <label for="validationCustom04" class="form-label text-secondary">Número</label>
-        <input type="text" name="input_numero" class="form-control"  value="<?= $user['numero'] ?? '' ?>" required>
-
-    </div>
-    <div class="col-md-6">
-        <label for="validationCustom05" class="form-label text-secondary">Perfil</label>
-        <input type="text" name="input_perfil" class="form-control"  value="<?= $user['perfil'] ?? '' ?>" required>
-    <div class="modal-footer">
-        <input type="submit" class="btn btn-primary" name="btnSend" id="btnSend" value="Atualizar">
-    </div>
-</form>
+            <label for="validationCustom05" class="form-label text-secondary">Perfil</label>
+            <input type="text" name="input_perfil" class="form-control"  value="<?= $user['perfil'] ?? '' ?>" required>
+        <div class="modal-footer">
+            <input type="submit" class="btn btn-primary" name="btnSend" id="btnSend" value="Atualizar">
+        </div>
+    </form>
+</body>
--- src/meusRecebimentos.php
+++ src/meusRecebimentos.php
@@ -17,7 +17,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Beneficiário</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
     <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../style/style.css" rel="stylesheet">
